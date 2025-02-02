65944f7209e1ee035202f6203e09f3645ac322fb
guentherloesel <email@guenther-loesel.de>
2025-02-02 13:45:11 +0100

    adds some test values, updates fn version

--- myserverlessfn/handler.js
+++ myserverlessfn/handler.js
@@ -3,7 +3,9 @@
 module.exports = async (event, context) => {
   const result = {
     'body': JSON.stringify(event.body),
-    'content-type': event.headers["content-type"]
+    'content-type': event.headers["content-type"],
+    'method': event.httpMethod,
+    'mypayload': 'Hello World',
   }
 
   return context
